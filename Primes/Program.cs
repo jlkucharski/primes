8efacb104d880f4e7385434ba83ec9b674ff3103
J. K <j.l.kucharski@gmail.com>
2019-02-02 23:03:23 -0800

    Finding primes in bit range

--- Primes/Program.cs
+++ Primes/Program.cs
@@ -13,7 +13,8 @@ namespace Primes
 			{
 				{"Is a number prime?", () => {IsNumberPrime(); } },
 				{"Find primes in range", () => { FindPrimesInRange(); } },
-				{"Find n primes", () => { FindFirstNPrimes(); } }
+				{"Find n primes", () => { FindFirstNPrimes(); } },
+				{"Find primes in bit range", () => { FindPrimesInBitRange(); } }
 			});
 
 			menu.ProcessInput();
@@ -70,18 +71,40 @@ namespace Primes
 
 		}
 
+		static void FindPrimesInBitRange()
+		{
+			Console.WriteLine("Provide number of bits to look up");
+			var line = Console.ReadLine();
+			int bits = int.Parse(line);
+
+			BigInteger min = new BigInteger(1) << bits;
+			BigInteger max = (min << 1) - 1;
+
+			FindPrimes(min, max);
+		}
+
 		static void FindPrimesInRange()
 		{
-			Console.WriteLine("Provide max");
+			Console.WriteLine("Provide min (enter to use default 3)");
 			var line = Console.ReadLine();
+			BigInteger min = string.IsNullOrWhiteSpace(line) ? 3 : BigInteger.Parse(line);
+
+			Console.WriteLine("Provide max");
+			line = Console.ReadLine();
 			BigInteger max = BigInteger.Parse(line);
+
+			FindPrimes(min, max);
+		}
+
+		private static void FindPrimes(BigInteger min, BigInteger max)
+		{
 			BigInteger primeCount = 0;
 			Stopwatch stopwatch = new Stopwatch();
 
 			Console.WriteLine($"Trying to find primes lower or equal to {max}...");
 
 			stopwatch.Start();
-			for (BigInteger i = 3; i <= max; i += 2)
+			for (BigInteger i = min.IsEven ? min + 1 : min; i <= max; i += 2)
 			{
 				if (IsPrime(i))
 				{
@@ -92,16 +115,14 @@ namespace Primes
 			stopwatch.Stop();
 
 			Console.WriteLine();
-			Console.WriteLine($"Found {primeCount} primes smaller than {max} in {stopwatch.ElapsedMilliseconds}ms.");
+			Console.WriteLine($"Found {primeCount} primes in range from {min} to {max} in {stopwatch.ElapsedMilliseconds}ms.");
 		}
 
 		static bool IsPrime(BigInteger value)
 		{
-			if (value < 3) return false;
+			if (value < 3 || value.IsEven || value.IsPowerOfTwo) return false;
 
-			BigInteger maxj = value >> 1;
-
-			for (BigInteger j = 3; j < maxj; j += 2)
+			for (BigInteger j = 3; j < value >> 1; j += 2)
 			{
 				if (value % j == 0)
 				{
